9bc94dcc2d53d2300f6741f3d8e4dc450ea59521
johngary <johnzaharick@gmail.com>
2017-05-07 15:23:57 -0500

    Earthquake id numbers are now used instead of full titles to resolve edge case where the same earthquake was played twice or more whenever a title was modified by the USGS.  Variable names are also more self explanatory.

--- parseAtomFeed.py
+++ parseAtomFeed.py
@@ -5,20 +5,21 @@
 import feedparser
 import time
 from toneGenerator import playTone
-quakeID = " "
+firstQuakeID = " "
 
 try:
 	while True:
 		d = feedparser.parse('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom')
 		print d['feed']['title']
 		for idx, item in enumerate(d['entries']):
-			mystring = str(d['entries'][idx]['title'])
-			if mystring != quakeID: #check that the particular earthquake hasn't been played already.  The atom feed contains all quakes from the past hour.
-				print mystring
-				playTone(float(mystring.split(" ")[1]) * 100)
+			currentQuakeID = str(d['entries'][idx]['id']).split(":")[3] #id number of the current earthquake being processed
+			magnitude = float(str(d['entries'][idx]['title']).split(" ")[1]) #position 1 in the title is always the magnitude. eg: M 0.8 - 17km ESE of Anza, California
+			if currentQuakeID != firstQuakeID: #check that the particular earthquake hasn't been played already.  The atom feed contains all quakes from the past hour.
+				print str(d['entries'][idx]['title'])
+				playTone(magnitude * 100)
 			else:
 				break
-		quakeID = str(d['entries'][0]['title']) #the title of the first earthquake played this round is stored
+		firstQuakeID = str(d['entries'][0]['id']).split(":")[3] #the id number of the first earthquake played this round is stored
 		print "Ctrl-C to stop program"
 		print " "
 		time.sleep(300) #the USGS atom feed updates once every 5 minutes, so the script loops every five minutes
